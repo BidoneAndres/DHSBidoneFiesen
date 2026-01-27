e800171f4e0a8e19e7c872000f82a0b771661b1a
BidoneAndres <bidoneandres@gmail.com>
2026-01-26 23:09:19 -0300

    While funcionando

--- DHS/src/main/python/dhs/Walker.py
+++ DHS/src/main/python/dhs/Walker.py
@@ -34,9 +34,9 @@ class Walker (compiladoresVisitor) :
         #print (ctx.getChild(0).getText()+" - "+ctx.getChild(1))
         #return none
     
-    def visitBloque(self, ctx:compiladoresParser.BloqueContext):
-        print("Nuevo Programa")
-        return super().visitInstrucciones(ctx.getChild(1))
+   # def visitBloque(self, ctx:compiladoresParser.BloqueContext):
+    #    print("Nuevo Programa")
+     #   return super().visitInstrucciones(ctx.getChild(1))
 
     def visitTerminal(self, node):
         print(" ==> Token "+ node.getText())
@@ -54,7 +54,6 @@ class Walker (compiladoresVisitor) :
     def visitInstruccion(self, ctx:compiladoresParser.InstruccionContext):
     # Despacha a la instrucción específica (declaración, asignación, etc.)
         if ctx.puntoYComa():
-            print("si")
             return self.visitPuntoYComa(ctx.puntoYComa())
         elif ctx.iwhile():
             return self.visitIwhile(ctx.iwhile())
@@ -66,3 +65,45 @@ class Walker (compiladoresVisitor) :
             return self.visitFunc(ctx.func())
         elif ctx.IF():  
             return self.visitIf(ctx.if_())
+
+    def visitIwhile(self, ctx:compiladoresParser.IwhileContext):
+        print("=-"*20)
+        print("SE ENCONTRO UN WHILE")
+        print("=-"*20)
+        
+        if ctx.opal():
+            condicion = self.obtenerValor(ctx.opal())
+            print(f"Condición: {condicion}")
+        
+        print("Cuerpo del while:")
+
+        if ctx.bloque():
+            self.visitBloque(ctx.bloque())
+        else:
+            for i in range(ctx.getChildCount()):
+                child = ctx.getChild(i)
+                if hasattr(child, 'getRuleIndex') and isinstance(child, compiladoresParser.InstruccionContext):
+                    self.visit(child)
+        
+        print("=-"*20)
+
+
+
+    def visitAsignacion(self, ctx:compiladoresParser.AsignacionContext):
+        print("=-"*20)
+        print("SE ENCONTRO UNA ASIGNACION")
+        print("=-"*20)
+        variable = ctx.ID().getText()
+        if ctx.opal():
+            # Procesa la expresión/operación
+            valor = self.obtenerValor(ctx.opal())
+            print(variable +" = " + f" {valor}")
+        elif ctx.char():
+            # Procesa el carácter
+            char_valor = ctx.char().getText()
+            print(variable +" = " + f" {char_valor}")
+        print("=-"*20)
+
+    def obtenerValor(self, ctx):
+        """Extrae el texto/valor de una operación o expresión"""
+        return ctx.getText()
